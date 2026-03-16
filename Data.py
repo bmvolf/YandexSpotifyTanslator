ebefdf196b43c6c363845abd6572abc6c1d5c586
bmvolgin@gmail.com <bmvolgin@gmail.com>
2026-03-16 11:54:44 +0300

    try

--- Data.py
+++
@@ -1,5 +0,0 @@
-Token = 'y0__xDElMPLBRje-AYgv9r3yBV4z7colR5wN01GkBcHLDjxb1w2pA'
-Playlist = 0
-User = ''
-client_id = 'a9d6e2768a5d4cfa82c1d81aee5a935c'
-client_secret = '7ed8610f713c42a989b902aafa8b0dee'
